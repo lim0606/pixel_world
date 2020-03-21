dbdbca218f1feb7d372be9e5b6828d565b762dbb
Jae Hyun Lim <lim0606@gmail.com>
2020-03-21 18:10:34 -0400

    update env to continuous control

--- pixel_world/__init__.py
+++ pixel_world/__init__.py
@@ -12,30 +12,30 @@ dirname = os.path.dirname(__file__)
 register(
     id='PixelWorld-v0',
     entry_point='pixel_world.env_utils:PixelWorld',
-    kwargs={'reward_mapping':navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room1_small.txt",'from_string':False}
+    kwargs={'reward_mapping':navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room1_small.txt",'from_string':False, 'actions':'2d_continuous'}
 )
 register(
     id='PixelWorld-v1',
     entry_point='pixel_world.env_utils:PixelWorld',
-    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room2_small.txt",'from_string':False}
+    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room2_small.txt",'from_string':False, 'actions':'2d_continuous'}
 )
 register(
     id='PixelWorld-v2',
     entry_point='pixel_world.env_utils:PixelWorld',
-    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room3_small.txt",'from_string':False}
+    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room3_small.txt",'from_string':False, 'actions':'2d_continuous'}
 )
 register(
     id='PixelWorld-v3',
     entry_point='pixel_world.env_utils:PixelWorld',
-    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room4_small.txt",'from_string':False}
+    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room4_small.txt",'from_string':False, 'actions':'2d_continuous'}
 )
 register(
     id='PixelWorld-v4',
     entry_point='pixel_world.env_utils:PixelWorld',
-    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room5_medium.txt",'from_string':False}
+    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room5_medium.txt",'from_string':False, 'actions':'2d_continuous'}
 )
 register(
     id='PixelWorld-v5',
     entry_point='pixel_world.env_utils:PixelWorld',
-    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room5_medium_walls.txt",'from_string':False}
+    kwargs={'reward_mapping':noisy_navigation_alphabet(),'world_map':dirname+"/"+"maps"+sep+"room5_medium_walls.txt",'from_string':False, 'actions':'2d_continuous'}
 )
